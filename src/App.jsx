2b1c17f1b42fb827ec5ce4fa38e0942db971c437
Nics2Extreme <J.Nics016@gmail.com>
2023-04-12 22:13:00 +0800

    update for protected views

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Landing from "./components/Landing";
+import RequireAuth from "./components/RequireAuth";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import Inquiry from "./views/Inquiry";
+import Admin from "./views/Admin";
+import Adminlogin from "./views/Adminlogin";
+import Tenant from "./views/Tenant";
+import Unauthorized from "./views/Unauthorized";
+
+const ROLES = {
+  Tenant: 2001,
+  Admin: 5150,
+};
 
 function App() {
   //https://www.youtube.com/watch?v=brcHK3P6ChQ
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/*public routes*/}
+        <Route path="/" element={<Landing />} />
+        <Route path="/inquire" element={<Inquiry />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/adminlogin" element={<Adminlogin />} />
+
+        {/*protected routes*/}
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="/admin" element={<Admin />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/inquire" element={<Inquiry />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Tenant]} />}>
+          <Route path="/tenant" element={<Tenant />} />
+        </Route>
+
+        <Route path="*" element={<Unauthorized />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/views/Tenant.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tenant() {
+  return <h1>Welcome Tenant</h1>;
+}
+
+export default Tenant;
